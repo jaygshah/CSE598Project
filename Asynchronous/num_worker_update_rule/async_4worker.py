2941269078cda117904b465a232bf0cf338fce5a
jayshah <jaygshah22@gmail.com>
2020-04-17 23:19:35 -0700

    to pull new updates

---
+++ Asynchronous/num_worker_update_rule/async_4worker.py
@@ -0,0 +1,180 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from filelock import FileLock
+import numpy as np
+import time
+
+import ray
+
+
+def get_data_loader():
+    """Safely downloads data. Returns training/validation set dataloader."""
+    mnist_transforms = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.1307, ), (0.3081, ))])
+
+    # We add FileLock here because multiple workers will want to
+    # download data, and this may cause overwrites since
+    # DataLoader is not threadsafe.
+    with FileLock(os.path.expanduser("~/data.lock")):
+        train_loader = torch.utils.data.DataLoader(
+            datasets.MNIST(
+                "~/data",
+                train=True,
+                download=True,
+                transform=mnist_transforms),
+            batch_size=128,
+            shuffle=True)
+        test_loader = torch.utils.data.DataLoader(
+            datasets.MNIST("~/data", train=False, transform=mnist_transforms),
+            batch_size=128,
+            shuffle=True)
+    return train_loader, test_loader
+
+
+def evaluate(model, test_loader):
+    """Evaluates the accuracy of the model on a validation dataset."""
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            # This is only set to finish evaluation faster.
+            if batch_idx * len(data) > 1024:
+                break
+            outputs = model(data)
+            _, predicted = torch.max(outputs.data, 1)
+            total += target.size(0)
+            correct += (predicted == target).sum().item()
+    return 100. * correct / total
+
+
+class ConvNet(nn.Module):
+    """Small ConvNet for MNIST."""
+
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 3, kernel_size=3)
+        self.fc = nn.Linear(192, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 3))
+        x = x.view(-1, 192)
+        x = self.fc(x)
+        return F.log_softmax(x, dim=1)
+
+    def get_weights(self):
+        return {k: v.cpu() for k, v in self.state_dict().items()}
+
+    def set_weights(self, weights):
+        self.load_state_dict(weights)
+
+    def get_gradients(self):
+        grads = []
+        for p in self.parameters():
+            grad = None if p.grad is None else p.grad.data.cpu().numpy()
+            grads.append(grad)
+        return grads
+
+    def set_gradients(self, gradients):
+        for g, p in zip(gradients, self.parameters()):
+            if g is not None:
+                p.grad = torch.from_numpy(g)
+
+@ray.remote
+class ParameterServer(object):
+    def __init__(self, lr):
+        self.model = ConvNet()
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
+
+    def apply_gradients(self, *gradients):
+        summed_gradients = [
+            # np.stack(gradient_zip).mean(axis=0)
+            
+            # originally it had sum as implemented 
+            # below here
+            np.stack(gradient_zip).sum(axis=0) 
+            for gradient_zip in zip(*gradients)
+        ]
+        self.optimizer.zero_grad()
+        self.model.set_gradients(summed_gradients)
+        self.optimizer.step()
+        return self.model.get_weights()
+
+    def get_weights(self):
+        return self.model.get_weights()
+
+
+@ray.remote
+class DataWorker(object):
+    def __init__(self):
+        self.model = ConvNet()
+        self.data_iterator = iter(get_data_loader()[0])
+
+    def compute_gradients(self, weights):
+        self.model.set_weights(weights)
+        try:
+            data, target = next(self.data_iterator)
+        except StopIteration:  # When the epoch ends, start a new epoch.
+            self.data_iterator = iter(get_data_loader()[0])
+            data, target = next(self.data_iterator)
+        self.model.zero_grad()
+        output = self.model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        return self.model.get_gradients()
+
+
+iterations = 500
+num_workers = 5
+
+ray.init(ignore_reinit_error=True)
+ps = ParameterServer.remote(0.03)
+workers = [DataWorker.remote() for i in range(num_workers)]
+
+model = ConvNet()
+test_loader = get_data_loader()[1]
+
+print("Running Asynchronous Parameter Server Training.")
+print("===============================================")
+
+current_weights = ps.get_weights.remote()
+
+gradients = {}
+for worker in workers:
+    gradients[worker.compute_gradients.remote(current_weights)] = worker
+
+start_time_2 = time.time()
+
+for i in range((iterations * num_workers) // 4):
+    ready_gradient_list = []
+    ready_gradient_list, _ = ray.wait(list(gradients), num_returns=4)
+    ready_gradient_id1 = ready_gradient_list[0]
+    ready_gradient_id2 = ready_gradient_list[1]
+    ready_gradient_id3 = ready_gradient_list[2]
+    ready_gradient_id4 = ready_gradient_list[3]
+    worker1 = gradients.pop(ready_gradient_id1)
+    worker2 = gradients.pop(ready_gradient_id2)
+    worker3 = gradients.pop(ready_gradient_id3)
+    worker4 = gradients.pop(ready_gradient_id4)
+
+    # Compute and apply gradients.
+    current_weights = ps.apply_gradients.remote(
+        *[ready_gradient_id1, ready_gradient_id2, ready_gradient_id3,
+        ready_gradient_id4])
+    gradients[worker1.compute_gradients.remote(current_weights)] = worker1
+    gradients[worker2.compute_gradients.remote(current_weights)] = worker2
+    gradients[worker3.compute_gradients.remote(current_weights)] = worker3
+    gradients[worker4.compute_gradients.remote(current_weights)] = worker4
+
+    if i % 100 == 0:
+        # Evaluate the current model after every 100 updates.
+        model.set_weights(ray.get(current_weights))
+        accuracy = evaluate(model, test_loader)
+        print("Iter {}: \taccuracy is {:.1f}".format(i, accuracy))
+
+print("Final accuracy for Asynchronous is {:.1f}.".format(accuracy))
+print('Total time for Asynchronous: {0} seconds'.format(time.time() - start_time_2))
